e83f7025133442fb755441a7ad0313e68fd0e3d7
pigulolkm <pigulolkm@gmail.com>
2013-12-04 17:48:22 +0800

    First Commit - Can read codes

---
+++ src/com/example/zbardemo/ZBarDemoActivity.java
@@ -0,0 +1,40 @@
+package com.example.zbardemo;
+
+import com.example.zbardemo.CameraTestActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ZBarDemoActivity extends Activity {
+	
+	Button btnScan;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_zbar_demo);
+		
+		btnScan = (Button)findViewById(R.id.BtnScan);
+		btnScan.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(ZBarDemoActivity.this, CameraTestActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.zbar_demo, menu);
+		return true;
+	}
+
+}
